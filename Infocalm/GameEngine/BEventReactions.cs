0d84e4d8830976ad7743f725e78370f155d8a5e9
Clayton Sadler <csadler@paychex.com>
2024-06-27 13:21:49 -0400

    Exploring possible class config

--- Infocalm/GameEngine/BEventReactions.cs
+++ Infocalm/GameEngine/BEventReactions.cs
@@ -11,14 +11,16 @@ namespace GameEngine
         public string ReactionName { get; private set; }
         public string TriggeringOption { get; private set; }
         public string TriggeredEvent { get; private set; }
+        public string TriggeredEventType { get; private set; }
         public List<string> ReactionMessages { get; private set; }
         public List<ResourceReaction> ResourceReactions {  get; private set; } 
 
-        public BEventReactions(string reactionName, string triggeringOption,  string triggeredEvent, List<string> reactionMessages, List<ResourceReaction> resourceReaction)
+        public BEventReactions(string reactionName, string triggeringOption,  string triggeredEvent, string triggeredEventType, List<string> reactionMessages, List<ResourceReaction> resourceReaction)
         {
             this.ReactionName = reactionName;
             this.TriggeringOption = triggeringOption;
             this.TriggeredEvent = triggeredEvent;
+            this.TriggeredEventType = triggeredEventType;
             this.ReactionMessages = reactionMessages;
             this.ResourceReactions = resourceReaction;
         }
